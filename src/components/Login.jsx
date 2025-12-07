94d9443ef309a9ace5f14b1c133761ff155599d4
rohit <rohitmahto062@gmail.com>
2025-12-07 21:13:06 +0530

    Add login form with validation and toggle functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
 import Header from "./Header";
 import bgimg from "../assets/Background.jpg";
+import { useRef, useState } from "react";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [errMessage, setErrMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleSubmit = () => {
+    // Validation
+
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value
+    );
+    setErrMessage(message);
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div>
       <div className="relative">
@@ -15,30 +37,51 @@ const Login = () => {
         </div>
         <div className="p-4 flex justify-center items-center h-screen">
           <form
+            onSubmit={(e) => e.preventDefault()}
             action=""
             className="bg-black/70 relative p-15 m-5 flex flex-col w-1/3"
           >
-            <h2 className="font-extrabold text-3xl my-5">Sign in</h2>
+            <h2 className="font-extrabold text-3xl my-5">
+              {isSignIn ? "Sign in" : "Sign Up"}
+            </h2>
+            {!isSignIn && (
+              <input
+                className="px-2 py-3 my-3 mx- border border-gray-300 rounded-md bg-gray-900/60 text-white text-sm"
+                type="text"
+                placeholder="Full Name"
+              />
+            )}
             <input
+              ref={email}
               className="px-2 py-3 my-3 mx- border border-gray-300 rounded-md bg-gray-900/60 text-white text-sm"
               type="text"
-              placeholder="Email or Mobile number"
+              placeholder="Email"
             />
             <input
+              ref={password}
               className="px-2 py-3 my-3 border border-gray-300 rounded-md bg-gray-900/60 text-white text-sm"
               type="password"
               placeholder="Password"
             />
-            <button className="p-2 my-3 bg-red-600 rounded font-bold cursor-pointer hover:bg-red-500 duration-200">
-              Sign in
+            <p className="text-red-600 font-bold text-sm text-center">
+              {errMessage}
+            </p>
+            <button
+              className="p-2 my-3 bg-red-600 rounded font-bold cursor-pointer hover:bg-red-500 duration-200"
+              onClick={handleSubmit}
+            >
+              {isSignIn ? "Sign in" : "Sign Up"}
             </button>
             <span className="flex w-full items-center justify-center font-bold text-lg uppercase">
               or
             </span>
             <p className="flex w-full items-center justify-center">
-              Don't have an account? &nbsp;
-              <span className="cursor-pointer hover:text-blue-700 hover:underline duration-200">
-                Sign up now
+              {isSignIn ? "New to Netflix?" : "Already have an account?"} &nbsp;
+              <span
+                className="cursor-pointer hover:text-blue-700 hover:underline duration-200"
+                onClick={toggleSignInForm}
+              >
+                {isSignIn ? "Sign up now" : "Sign in here"}
               </span>
             </p>
           </form>
